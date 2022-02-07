8aecc1afc1f020e07acfe761db9f9c123229009c
Anna Suuronen <annasuuronen@icloud.com>
2022-02-07 20:33:47 +0200

    osan2b tehtävä 2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,8 +1,11 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1, phone: '0451222222' }
+    { name: 'Arto Hellas', id: 1, number: '0451222222' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -12,7 +15,7 @@ const App = () => {
     const nameObject = {
       name: newName,
       id: persons.length + 1,
-      phone: newNumber
+      number: newNumber
     }
     
     let canBeAdded = true
@@ -42,26 +45,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      
-      <form onSubmit={addName}>
-        <h2>add a new</h2>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/> <br/>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {persons.map(person =>
-          <p key={person.id}> {person.name} {person.phone} </p>  
-        )}
-      </div>
+      <Filter/>
+      <h3>add a new</h3>
+      <PersonForm 
+        addName={addName} 
+        newName={newName} 
+        newNumber={newNumber} 
+        handleNameChange={handleNameChange} 
+        handleNumberChange={handleNumberChange} 
+      />
+      <h3>Numbers</h3>
+      <Persons persons={persons}/>
     </div>
   )
-
 }
 
 export default App
